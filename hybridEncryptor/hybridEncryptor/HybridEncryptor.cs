e0774ce76a0c2e9c5c9567fcfe131fcbd6e3cb1d
Sander Elsen <sander.elsen@pandora.be>
2016-03-18 14:24:44 +0100

    encryption added

--- hybridEncryptor/hybridEncryptor/HybridEncryptor.cs
+++ hybridEncryptor/hybridEncryptor/HybridEncryptor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +12,47 @@ namespace hybridEncryptor
     {
         private DesEncryptor des;
         private RsaEncryptor rsa;
+        private SHA1 sha;
+
+        private byte[] byteFile;
+        private string stringFile;
+
+        private byte[] encryptedFile;
+        private byte[] encryptedDesKey;
+        private byte[] encryptedDesIV;
+
+        private byte[] fileHash;
+        private byte[] encryptedHash;
         public HybridEncryptor()
         {
             des = new DesEncryptor();
             rsa = new RsaEncryptor();
+        }
+        public void SetFileName(string filename)
+        {
+            byteFile = File.ReadAllBytes(filename);
+            stringFile = byteFile.ToString();
+        }
+        public void Encrypt(string publicOntvanger,string privateZender)
+        {
+            //encrypt file
+            encryptedFile = des.Encrpyt(stringFile);
+            //encrypt keys
+            rsa.SetKey(publicOntvanger);
+            encryptedDesKey = rsa.Encrypt(des.GetKey());
+            encryptedDesIV = rsa.Encrypt(des.GetIV());
+            //encrypt hash
+            fileHash = sha.ComputeHash(byteFile);
+            rsa.SetKey(privateZender);
+            encryptedHash = rsa.Encrypt(fileHash);
+        }
+        public string GetEncryptedFile()
+        {
+            return encryptedFile.ToString();
+        }
+        public string GetEncryptedHash()
+        {
+            return encryptedHash.ToString();
         }
     }
 }
